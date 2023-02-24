453da642e35759144dd7493f676d6575b316fc84
Ahmed Ramadan <ahmed.r.mostafa.90s@gmail.com>
2023-02-23 22:29:17 -0800

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'use puts to print a string'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str[] = "\"Programming is like building a multilingual puzzle";
+puts(str);
+return (0);
+}
