138babc4afd26fb2872fb84c6e39f92470fe7edb
Ahmed Ramadan <ahmed.r.mostafa.90s@gmail.com>
2023-03-05 02:05:53 -0800

    unrepeted combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+/**
+ * main - Entry point
+ * Description: 'use putchar to print'
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	int i, j, l1, l2;
+	l1 = l2 = 48;
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
+		{
+			if (i != j && i < j)
+			{
+			putchar(l1);
+			putchar(l2);
+				if (i != 8)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		l2++;
+		}
+	l1++;
+	l2 = 48;
+	}
+	putchar('\n');
+	return (0);
+}
