1ec5c3c5ce99a0bdcb19beb44e632033596e4d23
Ahmed Ramadan <ahmed.r.mostafa.90s@gmail.com>
2023-03-05 00:36:12 -0800

    one digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry point
+ * Description: 'use putchar to print'
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+int i, l = 48;
+for (i = 0 ; i < 10 ; i++)
+{
+putchar(l);
+putchar(',');
+putchar(' ');
+l++;
+}
+/*putchar('\n');*/
+return (0);
+}
