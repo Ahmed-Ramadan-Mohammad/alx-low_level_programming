2a6f4a2e295dd54679e0dd96f38ad5a73b874ea7
Ahmed Ramadan <ahmed.r.mostafa.90s@gmail.com>
2023-03-05 11:15:51 -0800

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+/**
+ * main - Entry point
+ * Description: 'use puts to print a string'
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+int i, l = 48;
+for (i = 0 ; i < 10 ; i++)
+{
+	putchar(l);
+	l++;
+}
+l = 97;
+for (i = 0 ; i < 6 ; i++)
+{
+	putchar(l);
+	l++;
+}
+putchar('\n');
+return (0);
+}
