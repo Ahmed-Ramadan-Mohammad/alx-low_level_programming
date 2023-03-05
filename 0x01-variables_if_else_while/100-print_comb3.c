59bd87c55bc1c4e215a43255a059d9b1a532b282
Ahmed Ramadan <ahmed.r.mostafa.90s@gmail.com>
2023-03-05 02:09:46 -0800

    comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,27 +6,27 @@
 #include <stdio.h>
 int main(void)
 {
-	int i, j, l1, l2;
-	l1 = l2 = 48;
-	for (i = 0 ; i < 10 ; i++)
-	{
-		for (j = 0 ; j < 10 ; j++)
-		{
-			if (i != j && i < j)
-			{
-			putchar(l1);
-			putchar(l2);
-				if (i != 8)
-				{
-				putchar(',');
-				putchar(' ');
-				}
-			}
-		l2++;
-		}
-	l1++;
-	l2 = 48;
-	}
-	putchar('\n');
-	return (0);
+int i, j, l1, l2;
+l1 = l2 = 48;
+for (i = 0 ; i < 10 ; i++)
+{
+for (j = 0 ; j < 10 ; j++)
+{
+if (i != j && i < j)
+{
+putchar(l1);
+putchar(l2);
+if (i != 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+l2++;
+}
+l1++;
+l2 = 48;
+}
+putchar('\n');
+return (0);
 }
