9abf2f2cb1c041d49d8f44527224f17b6744db41
Ahmed Ramadan <ahmed.r.mostafa.90s@gmail.com>
2023-03-07 09:56:06 -0800

    hour day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints minutes of day
+ * Return: no return
+ */
+void jack_bauer(void)
+{int m1, m2, h1, h2;
+for (h1 = '0'; h1 <= '2'; h1++)
+{
+	if (h1 == '0' || h1 == '1')
+	{
+		for (h2 = '0'; h2 <= '9'; h2++)
+		{
+			for (m1 = '0'; m1 <= '5'; m1++)
+			{
+				for (m1 = '0'; m1 <= '9'; m2++)
+				{
+					_putchar(h1);
+					_putchar(h2);
+					_putchar(':');
+					_putchar(m1);
+					_putchar(m2);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+	else
+	{
+		for (h2 = '0'; h2 <= '4'; h2++)
+		{
+			for (m1 = '0'; m1 <= '5'; m1++)
+			{
+				for (m1 = '0'; m1 <= '9'; m2++)
+				{
+					_putchar(h1);
+					_putchar(h2);
+					_putchar(':');
+					_putchar(m1);
+					_putchar(m2);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
+}
