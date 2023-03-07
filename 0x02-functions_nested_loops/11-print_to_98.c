05517b89e07381f52ff76a743b23981c3f4e01c0
Ahmed Ramadan <ahmed.r.mostafa.90s@gmail.com>
2023-03-07 11:17:04 -0800

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_to_98 - returns the absolute value of number it takes
+ * @n: the arguement number
+ * Return: no return
+ */
+void print_to_98(int n)
+{
+if (n > 98)
+{
+	while (n >= 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	printf("\n");
+}
+if (n <= 98)
+{
+	while (n <= 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	printf("\n");
+}
