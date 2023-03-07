cb58b61adb56f046af9fd77a4a13c1ff6c671eb6
Ahmed Ramadan <ahmed.r.mostafa.90s@gmail.com>
2023-03-07 12:16:51 -0800

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ for (x = 0; x <= 9; x++)
 {
 	_putchar(48);
 	_putchar(',');
-	for (y = 1; y <= 9; y++)
+	for (y = 1; y < 9; y++)
 	{
 		z = x * y;
 		if (z > 9)
@@ -25,9 +25,25 @@ for (x = 0; x <= 9; x++)
 		else if (z <= 9)
 		{
 			_putchar(32);
+			_putchar(32);
 			_putchar(48 + z);
 			_putchar(',');
 		}
+	}
+	z = x * y;
+	if (z > 9)
+	{
+		u = z % 10;
+		d = (z - u) / 10;
+		_putchar(32);
+		_putchar(48 + d);
+		_putchar(48 + u);
+	}
+	else if (z <= 9)
+	{
+		_putchar(32);
+		_putchar(32);
+		_putchar(48 + z);
 	}
 	_putchar('\n');
 }
