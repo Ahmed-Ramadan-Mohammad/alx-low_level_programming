fe7ca1389263616c3333c31f379237988268b40a
Ahmed Ramadan <ahmed.r.mostafa.90s@gmail.com>
2023-03-07 11:31:49 -0800

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,22 +7,26 @@
  */
 void print_to_98(int n)
 {
+int h;
 if (n > 98)
 {
-	while (n >= 98)
+	h = n
+	while (h > 98)
 	{
-		printf("%d, ", n);
-		n--;
+		printf("%d, ", h);
+		h--;
 	}
-	printf("\n");
+	printf("%d\n", h);
 }
-if (n <= 98)
+else if (n < 98)
 {
-	while (n <= 98)
+	while (n < 98)
 	{
 		printf("%d, ", n);
 		n++;
 	}
-	printf("\n");
+	printf("%d\n", n);
 }
+else if (n == 98)
+	printf("%d\n", n);
 }
