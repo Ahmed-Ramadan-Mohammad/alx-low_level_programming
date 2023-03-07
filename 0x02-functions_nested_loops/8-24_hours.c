84701ecf2bbbf1c1b9c088561eb13d15c1d37aed
Ahmed Ramadan <ahmed.r.mostafa.90s@gmail.com>
2023-03-07 10:12:19 -0800

    hour

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,41 +8,23 @@ void jack_bauer(void)
 {int m1, m2, h1, h2;
 for (h1 = '0'; h1 <= '2'; h1++)
 {
-	if (h1 == '0' || h1 == '1')
+	for (h2 = '0'; h2 <= '9'; h2++)
 	{
-		for (h2 = '0'; h2 <= '9'; h2++)
+		for (m1 = '0'; m1 <= '5'; m1++)
 		{
-			for (m1 = '0'; m1 <= '5'; m1++)
+			for (m2 = '0'; m2 <= '9'; m2++)
 			{
-				for (m2 = '0'; m2 <= '9'; m2++)
-				{
-					_putchar(h1);
-					_putchar(h2);
-					_putchar(':');
-					_putchar(m1);
-					_putchar(m2);
-					_putchar('\n');
-				}
+				if (h1 >= '2' && h2 >= '4')
+					break;
+				_putchar(h1);
+				_putchar(h2);
+				_putchar(':');
+				_putchar(m1);
+				_putchar(m2);
+				_putchar('\n');
 			}
 		}
 	}
-	else
-	{
-		for (h2 = '0'; h2 <= '4'; h2++)
-		{
-			for (m1 = '0'; m1 <= '5'; m1++)
-			{
-				for (m2 = '0'; m2 <= '9'; m2++)
-				{
-					_putchar(h1);
-					_putchar(h2);
-					_putchar(':');
-					_putchar(m1);
-					_putchar(m2);
-					_putchar('\n');
-				}
-			}
-		}
-	}
+
 }
 }
