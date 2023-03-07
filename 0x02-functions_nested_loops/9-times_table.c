ab043a6866d2228c90fcbae628443566e6c9361d
Ahmed Ramadan <ahmed.r.mostafa.90s@gmail.com>
2023-03-07 10:36:14 -0800

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - prints minutes of day
+ * Return: no return
+ */
+void times_table(void)
+{int x, y, z, u, d;
+for (x = 0; x <= 9; x)
+{
+	for (y = 0; y <= 9; y++)
+	{
+		z = x * y;
+		if (z > 9)
+		{
+			u = z % 10;
+			d = (z - u) / 10;
+			_putchar(32);
+			_putchar(48 + d);
+			_putchar(48 + u);
+			_putchar(',');
+		}
+		else
+		{
+			_putchar(32);
+			_putchar(48 + z);
+			_putchar(',');
+		}
+	}
+	_putchar('\n');
+}
+}
