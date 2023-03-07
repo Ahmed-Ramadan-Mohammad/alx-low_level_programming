98e6b6514b53ce73b9d2716f1045d89ce8049f66
Ahmed Ramadan <ahmed.r.mostafa.90s@gmail.com>
2023-03-07 11:00:08 -0800

    add fn

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - returns the absolute value of number it takes
+ * @a: the arguement number 1
+ * @l: the arguement number 2
+ * Return: add value
+ */
+int add(int a, int l)
+{
+int r = a + l;
+return (r);
+}
