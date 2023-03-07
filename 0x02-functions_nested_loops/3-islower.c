842984a9262e51a699f2f47559b2fbe937f8ef62
Ahmed Ramadan <ahmed.r.mostafa.90s@gmail.com>
2023-03-07 08:11:30 -0800

    lowercase check

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
 /**
- * _islower - Entry point
- * Description: 'check lower case'
+ * _islower - returns 1 if c is a lowercase
+ * in other cases returns 0
+ * @c: the charcter in ascii code
+ * Return: Always 1 (Success)
  */
 int _islower(int c)
 {
+int r;
 if (c >= 'a' && c <= 'z')
-	return (1);
+r = 1;
 else
-	return (0);
+r = 0;
+return (r);
 }
