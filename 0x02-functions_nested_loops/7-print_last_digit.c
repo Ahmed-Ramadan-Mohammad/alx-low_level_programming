aefd82e1c33215c324e26bbc4fe812520e927f3e
Ahmed Ramadan <ahmed.r.mostafa.90s@gmail.com>
2023-03-07 09:09:15 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - returns the last digit of the
+ * number it takes
+ * @n: the arguement number
+ * Return: abs value
+ */
+int print_last_digit(int n)
+{
+int r;
+if (n >= 0)
+{
+	r = n % 10;
+}
+else if (n < 0)
+{
+	r = -1 * n;
+	r = r % 10;
+}
+return (r);
+}
