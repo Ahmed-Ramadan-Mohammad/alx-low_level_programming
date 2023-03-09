861869380759647f5392ee25413a2df2e3c53dcb
Ahmed Ramadan <ahmed.r.mostafa.90s@gmail.com>
2023-03-09 01:26:11 -0800

    solved betty

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * pow_recursion - returns the factorial of agiven int number
+ * _pow_recursion - returns the factorial of agiven int number
  * @x: the arguement no.1 base number
  * @y: the argument no.2 power number
  * Return: the result of power operation & -1 at error
