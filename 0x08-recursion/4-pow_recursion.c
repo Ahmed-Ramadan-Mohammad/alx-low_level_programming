e06577e63d28e3a8481c3077248d1bb8725cf8b8
Ahmed Ramadan <ahmed.r.mostafa.90s@gmail.com>
2023-03-09 01:24:40 -0800

    power fn added

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * pow_recursion - returns the factorial of agiven int number
+ * @x: the arguement no.1 base number
+ * @y: the argument no.2 power number
+ * Return: the result of power operation & -1 at error
+ */
+int _pow_recursion(int x, int y)
+{
+int res;
+if (y < 0)
+	return (-1);
+if (y == 0)
+	return (1);
+res = x * _pow_recursion(x, --y);
+return (res);
+}
