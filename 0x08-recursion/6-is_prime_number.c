62191149f7a399319773321b4942670cb16f17a6
Ahmed Ramadan <ahmed.r.mostafa.90s@gmail.com>
2023-03-09 08:31:36 -0800

    modified to betty

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,21 +1,27 @@
 #include "main.h"
 
+/**
+ * prime_check - identifies the given number is a prime or not
+ * @a: the arguement given number
+ * @i: the argument 2
+ * Return: 1 if prime and 0 if not
+ */
 int prime_check(int a, int i)
 {
 if (a == i)
-        return (1);
-if (a % i != 0)
+	return (1);
+if ((a % i) != 0)
 {
-        int r2;
-        r2 = prime_check (a, (i+1));
-        return (r2);
+	int r2;
+
+	r2 = prime_check(a, (i + 1));
+	return (r2);
 }
 else
 {
-        return (0);
-}       
+	return (0);
 }
-
+}
 /**
  * is_prime_number - identifies the given number is a prime or not
  * @n: the arguement given number
@@ -31,6 +37,7 @@ else
 {
 	int i = 2;
 	int r2;
+
 	r2 = prime_check(n, i);
 	return (r2);
 }
