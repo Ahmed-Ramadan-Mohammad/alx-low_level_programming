c0aeceba85eb3de38ad604345a5a544cfec32aa1
Ahmed Ramadan <ahmed.r.mostafa.90s@gmail.com>
2023-03-09 01:00:18 -0800

    factorial fn added

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of agiven int number
+ * @n: the arguement
+ * Return: the factorial of agiven int number & -1 at error
+ */
+int factorial(int n)
+{
+if (n < 0)
+	return (-1);
+if (n == 0)
+	return (1);
+int fact = n * fact(n - 1);
+return (fact);
+}
